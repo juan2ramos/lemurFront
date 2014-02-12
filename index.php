7cab55fa280ac18d41fa5139eb0eedf14fefc7a7
juan2ramos <juan2ramos@gmail.com>
2014-02-12 18:28:21 -0500

    Archivos generales

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> 
+<html lang="es">
+    <head> 
+
+        <title>Lemur</title>
+
+        <!-- Meta -->
+        <meta charset="utf-8">
+        <meta name="author" content="juan2ramos" />       
+        <meta name="description" content="Inicio" />       
+        <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1" />
+
+        <link rel="shortcut icon" href="./favicon.ico" />
+        <script src="js/prefixfree.min.js"></script>
+        <!-- Estilos -->
+
+        <link rel="stylesheet" href="css/normalize.css" />
+        <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="css/style.css" />
+
+
+        
+    </head>
+    <body>
+
+        <header>
+            <nav>
+                
+            lemur
+            </nav>
+          
+
+
+        </header>
+       
+    </body>
+    <!-- JavaScript -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="js/script.js"></script>
+</html>
