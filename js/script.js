62dfc6678cd67f5772da2dba939c48e87606df17
juan2ramos <juan2ramos@gmail.com>
2014-02-14 15:59:32 -0500

    preguntas

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
 $(function() {
 
+  var allPanels = $('.accordion > dd').hide();
+    
+  $('.accordion > dt > .expand').click(function() {
+      $this = $(this);
+      $target =  $this.parent().next();      
+      
+        
+      if($this.hasClass('open')){        
+          $this.removeClass('open');
+          $this.css("backgroundImage", "url(images/more.png)");
+          allPanels.removeClass('active').slideUp();
+      }
+      else{
+        $this.addClass('open');  
+        if(!$target.hasClass('active')){
+           allPanels.removeClass('active').slideUp();
+           $target.addClass('active').slideDown();
 
+           $('.expand').css("backgroundImage", "url(images/more.png)");
+           $this.css("backgroundImage", "url(images/menos.png)");
+        }
+      }
+    return false;
+  });
 
 });
---
+++ views/preguntas.php
@@ -0,0 +1,43 @@
+			<div class="preguntas">
+		      	<h2>¿Tienes preguntas?</h2>
+		      	<dl class="accordion">
+
+					<dt><a href="" class="expand">¿Que es Lemur Studio?</a></dt>
+					<dd>
+						Dolor sit amet, consectetur adipiscing elit. Aliquam vitae fringilla augue. 
+						Maecenas in lectus lorem. In et accumsan mi. Aenean vestibulum nisl eu arcu 
+						viverra iaculis. In hac habitasse platea dictumst. In vehicula diam et 
+						mauris imperdiet aliquet.
+					</dd>
+
+					<dt><a href="" class="expand">¿Cómo puedo participar?</a></dt>
+					<dd>
+						Dolor sit amet, consectetur adipiscing elit. Aliquam vitae fringilla augue. 
+						Maecenas in lectus lorem. In et accumsan mi. Aenean vestibulum nisl eu arcu 
+						viverra iaculis. In hac habitasse platea dictumst. In vehicula diam et 
+						mauris imperdiet aliquet.
+					</dd>
+
+					<dt><a href="" class="expand">¿Porqué este negocio?</a></dt>
+					<dd>
+						Dolor sit amet, consectetur adipiscing elit. Aliquam vitae fringilla augue. 
+						Maecenas in lectus lorem. In et accumsan mi. Aenean vestibulum nisl eu arcu 
+						viverra iaculis. In hac habitasse platea dictumst. In vehicula diam et 
+						mauris imperdiet aliquet.
+					</dd>
+
+					<dt><a href="" class="expand">¿Cómo funciona el sistema de regalías?</a></dt>
+					<dd>
+						Dolor sit amet, consectetur adipiscing elit. Aliquam vitae fringilla augue. 
+						Maecenas in lectus lorem. In et accumsan mi. Aenean vestibulum nisl eu arcu 
+						viverra iaculis. In hac habitasse platea dictumst. In vehicula diam et 
+						mauris imperdiet aliquet.
+					</dd>
+
+					<dt><a href="terminos-y-condiciones" >Términos y Condiciones</a></dt>
+
+
+					<dt><a href="contacto" >Más preguntas</a></dt>
+
+				</dl>
+		    </div>
