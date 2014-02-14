5256d3d30b79de5ccfb0f53939dbff8b20916779
juan2ramos <juan2ramos@gmail.com>
2014-02-14 11:47:44 -0500

    trabaja, terminos

---
+++ views/terminos-y-condiciones.php
@@ -0,0 +1,69 @@
+			<div class="terminos-y-condiciones">
+		      <h2>Términos y Condiciones</h2>
+		      <p>
+			      Augue vut turpis scelerisque sed in mus dignissim porttitor integer 
+			      tempor quis! Scelerisque, enim aliquet, dapibus velit, habitasse 
+			      rhoncus? Scelerisque! Ac nisi sagittis elit. Pulvinar nisi. Augue 
+			      tincidunt platea tincidunt porttitor enim. Elit rhoncus odio amet, 
+			      tempor porttitor nunc. Natoque. Pulvinar platea rhoncus odio? Montes 
+			      nisi sit magna, auctor cum? Aliquet, enim pid ultrices, ultricies 
+			      lacus platea, dis facilisis ridiculus. Cras porttitor, adipiscing, 
+			      porttitor placerat lundium adipiscing? Nunc ut ridiculus porttitor 
+			      quis phasellus dolor dapibus parturient dolor a augue mus! Egestas 
+			      porttitor a porttitor urna, nec pulvinar lacus parturient in! Cras 
+			      proin vut in habitasse porta hac sociis lorem odio.!
+		      </p>
+		      <p>
+			      Augue vut turpis scelerisque sed in mus dignissim porttitor integer 
+			      tempor quis! Scelerisque, enim aliquet, dapibus velit, habitasse 
+			      rhoncus? Scelerisque! Ac nisi sagittis elit. Pulvinar nisi. Augue 
+			      tincidunt platea tincidunt porttitor enim. Elit rhoncus odio amet, 
+			      tempor porttitor nunc. Natoque. Pulvinar platea rhoncus odio? Montes 
+			      nisi sit magna, auctor cum? Aliquet, enim pid ultrices, ultricies 
+			      lacus platea, dis facilisis ridiculus. Cras porttitor, adipiscing, 
+			      porttitor placerat lundium adipiscing? Nunc ut ridiculus porttitor 
+			      quis phasellus dolor dapibus parturient dolor a augue mus! Egestas 
+			      porttitor a porttitor urna, nec pulvinar lacus parturient in! Cras 
+			      proin vut in habitasse porta hac sociis lorem odio.!
+		      </p>
+		      <p>
+			      Augue vut turpis scelerisque sed in mus dignissim porttitor integer 
+			      tempor quis! Scelerisque, enim aliquet, dapibus velit, habitasse 
+			      rhoncus? Scelerisque! Ac nisi sagittis elit. Pulvinar nisi. Augue 
+			      tincidunt platea tincidunt porttitor enim. Elit rhoncus odio amet, 
+			      tempor porttitor nunc. Natoque. Pulvinar platea rhoncus odio? Montes 
+			      nisi sit magna, auctor cum? Aliquet, enim pid ultrices, ultricies 
+			      lacus platea, dis facilisis ridiculus. Cras porttitor, adipiscing, 
+			      porttitor placerat lundium adipiscing? Nunc ut ridiculus porttitor 
+			      quis phasellus dolor dapibus parturient dolor a augue mus! Egestas 
+			      porttitor a porttitor urna, nec pulvinar lacus parturient in! Cras 
+			      proin vut in habitasse porta hac sociis lorem odio.!
+		      </p>
+		      <p>
+			      Augue vut turpis scelerisque sed in mus dignissim porttitor integer 
+			      tempor quis! Scelerisque, enim aliquet, dapibus velit, habitasse 
+			      rhoncus? Scelerisque! Ac nisi sagittis elit. Pulvinar nisi. Augue 
+			      tincidunt platea tincidunt porttitor enim. Elit rhoncus odio amet, 
+			      tempor porttitor nunc. Natoque. Pulvinar platea rhoncus odio? Montes 
+			      nisi sit magna, auctor cum? Aliquet, enim pid ultrices, ultricies 
+			      lacus platea, dis facilisis ridiculus. Cras porttitor, adipiscing, 
+			      porttitor placerat lundium adipiscing? Nunc ut ridiculus porttitor 
+			      quis phasellus dolor dapibus parturient dolor a augue mus! Egestas 
+			      porttitor a porttitor urna, nec pulvinar lacus parturient in! Cras 
+			      proin vut in habitasse porta hac sociis lorem odio.!
+		      </p>
+		      <p>
+			      Augue vut turpis scelerisque sed in mus dignissim porttitor integer 
+			      tempor quis! Scelerisque, enim aliquet, dapibus velit, habitasse 
+			      rhoncus? Scelerisque! Ac nisi sagittis elit. Pulvinar nisi. Augue 
+			      tincidunt platea tincidunt porttitor enim. Elit rhoncus odio amet, 
+			      tempor porttitor nunc. Natoque. Pulvinar platea rhoncus odio? Montes 
+			      nisi sit magna, auctor cum? Aliquet, enim pid ultrices, ultricies 
+			      lacus platea, dis facilisis ridiculus. Cras porttitor, adipiscing, 
+			      porttitor placerat lundium adipiscing? Nunc ut ridiculus porttitor 
+			      quis phasellus dolor dapibus parturient dolor a augue mus! Egestas 
+			      porttitor a porttitor urna, nec pulvinar lacus parturient in! Cras 
+			      proin vut in habitasse porta hac sociis lorem odio.!
+		      </p>
+
+		    </div>
---
+++ views/trabaja-en-lemur.php
@@ -0,0 +1,20 @@
+			<div class="trabaj-en-lemur">
+		      <h2>Contáctanos</h2>
+		      <p>
+			      Nuestro equipo es lo mejor de lo mejor, no nos importa tus años de experiencia, 
+			      tu edad ni tu religión, nos importa la pasión con la que seas capaz de trabajar, 
+			      compartir en equipo, actitud positiva y talento!
+		      </p>
+		      <p>
+		      	¿Te gustaría pertenecer a nuestro equipo? Envíanos tus datos, estaremos en contacto contigo.
+		      </p>
+		      <form class="contact-form">
+		        <label>Nombre<span>*</span></label>
+		        <input type="text">
+		        <label>apellidos<span>*</span></label>
+		        <input type="text">
+		        <label>Correo electrónico<span>*</span></label>
+		        <input type="text">	        
+		        <input class="sumbit" type="submit" value="Enviar">
+		      </form>
+		    </div>
