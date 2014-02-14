0fbcda12557846dcaa4fb7667ffc5f142ffdebda
juan2ramos <juan2ramos@gmail.com>
2014-02-14 08:41:01 -0500

    sube tu idea

--- views/categorias.php
+++ views/categorias.php
@@ -1,19 +1,19 @@
 				<div class="categorias">
                     <h2>Categorías</h2>
                     <p>Estas son las categorías vigentes para que puedas votar por tus ideas favoritas.</p>
-                    <a href="vota-por-una-idea.html" class="link activa">
+                    <a href="vota-por-una-idea" class="link activa">
                       <div class="contend-img"><img src="images/general.png"></div>
                       <div class="contend-p"><p>Nombre categoría</p></div>
                     </a>
-                    <a href="vota-por-una-idea.html" class="link activa">
+                    <a href="vota-por-una-idea" class="link activa">
                       <div class="contend-img"><img src="images/electrodomesticos.png"></div>
                       <div class="contend-p"><p>Nombre categoría</p></div>
                     </a>
-                    <a href="vota-por-una-idea.html" class="link">
+                    <a href="vota-por-una-idea" class="link">
                       <div class="contend-img"><img src="images/hogar.png"></div>
                       <div class="contend-p"><p>Nombre categoría</p></div>
                     </a>
-                    <a href="vota-por-una-idea.html" class="link">
+                    <a href="vota-por-una-idea" class="link">
                       <div class="contend-img"><img src="images/herramientas.png"></div>
                       <div class="contend-p"><p>Nombre categoría</p></div>
                     </a>
---
+++ views/vota-por-una-idea.php
@@ -0,0 +1,38 @@
+		<div class="vota-por-una-idea">
+	      <h2>Vota por una idea</h2>
+	      <p>Elige tu favorita y ayudala con tu voto, encontrarás información detallada de cada idea donde podrás dejar tus comentarios.</p>
+	      <ul>
+	        <li>
+	          <div class="izq">78</div>
+	          <div class="der">info</div>
+	        </li>
+	        <li>
+	          <div class="izq">345</div>
+	          <div class="der">info</div>
+	        </li>
+	        <li>
+	          <div class="izq">25</div>
+	          <div class="der">info</div>
+	        </li>
+	        <li>
+	          <div class="izq">45</div>
+	          <div class="der">info</div>
+	        </li>
+	        <li>
+	          <div class="izq">456</div>
+	          <div class="der">info</div>
+	        </li>
+	        <li>
+	          <div class="izq">3</div>
+	          <div class="der">info</div>
+	        </li>
+	        <li>
+	          <div class="izq">345</div>
+	          <div class="der">info</div>
+	        </li>
+	        <li>
+	          <div class="izq">345</div>
+	          <div class="der">info</div>
+	        </li>
+	      </ul>
+	    </div>
