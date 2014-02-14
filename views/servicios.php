9c6a311b0ded1ed7e8f4722003bd56c02661be70
juan2ramos <juan2ramos@gmail.com>
2014-02-14 11:29:33 -0500

    servicios

---
+++ views/servicios.php
@@ -0,0 +1,43 @@
+			<div class="servicios">
+		      <h2>Otros Servicios</h2>
+		      <ul>
+		      	<li>
+		      		<figure><img src="images/industrial.png"></figure>
+		      		<div class="contend-service">
+		      			<h3>Diseño Industrial</h3>
+				    	<p>
+					      Nuestro equipo está altamente capacitado para el desarrollo de nuevos 
+					      e innovadores productos, somos un equipo multidisciplinar en donde tanto 
+					      Diseñadores Industriales como profesionales de otras áreas trabajamos 
+					      por un mismo objetivo, dar lo mejor de nosotros, llegando más allá de las 
+					      expectativas de un cliente y dejando en alto el nombre del país.
+				      	</p>	
+			      	</div>	      		
+		      	</li>
+		      	<li>
+		      		<figure><img src="images/grafico.png"></figure>
+		      		<div class="contend-service">			      		
+			      		<h3>Diseño Gráfico</h3>
+				    	<p>
+					      Contamos con un equipo de profesionales en Diseño gráfico los cuales cuentan
+					       con gran experiencia en todos los campos de comunicación visual eficiente 
+					       que te aseguran un trabajo asertivo y realizado con profesionalismo.
+				      	</p>	
+			      	</div>	      		
+		      	</li>
+		      	<li>
+		      		<figure><img src="images/especiales.png"></figure>
+		      		<div class="contend-service">
+		      			<h3>Desarrollos Especiales</h3>
+				    	<p>
+					      Si requieres algún desarrollo de producto especial para tu empresa o para ti 
+					      no dudes en escribirnos, estaremos en contacto contigo para realizarlo juntos, 
+					      en Lemur Studio creemos en el trabajo participativo lo que asegura que el 
+					      resultado de éste sea exactamente lo que busca y necesita.
+				      	</p>		  
+			      	</div>    		
+		      	</li>
+		      </ul>
+
+
+		    </div>
