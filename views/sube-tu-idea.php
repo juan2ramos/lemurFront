c317aa83a62bbb121c4e24c71a28b55444aeb622
juan2ramos <juan2ramos@gmail.com>
2014-02-15 16:34:55 -0500

    cambios sube tu idea

--- views/sube-tu-idea.php
+++ views/sube-tu-idea.php
@@ -2,20 +2,39 @@
 		      <h2>Sube tu idea</h2>
 		      <p>Sube tu idea de producto ya y participa, dentro de poco podrías verla en las tiendas!</p>
 		      <form>
-		        <label>Dale un título a tu idea</label>
-		        <input placeholder="(con 2 o 3 palabras bastará)"class="" type="text">
+		      	<div id="input-contend">
+			        <label>Dale un título a tu idea</label>
+			        <span>30 caracteres restantes</span>
+			        <input placeholder="(con 2 o 3 palabras bastará)"class="" type="text">
+		        </div>
 		        <label>Categoría de tu idea</label>
 		        <input placeholder="(elige una categoría)"class="" type="text">
-		        <label>Describe tu idea en pocas palabras</label>
-		        <input placeholder="(Vende tu idea en un ascensor)"class="text" type="text">
-		        <label>Describe la oportunidad o problemática que descubriste</label>
-		        <input placeholder="(cuentanos del problema)"class="text" type="text">
-		        <label>Describe cómo solucionaste el problema</label>
-		        <input placeholder="(¿Cúal es tu solución?)"class="text" type="text">
+
+		        <div id="input-contend">
+			        <label>Describe tu idea en pocas palabras</label>
+			        <span>500 caracteres restante</span>
+			        <input placeholder="(Vende tu idea en un ascensor)"class="text" type="text">
+		        </div>
+		        <div id="input-contend">
+			        <label>Describe la oportunidad o problemática que descubriste</label>
+			        <span>1000 caracteres restante</span>
+			        <input placeholder="(cuentanos del problema)"class="text" type="text">
+		        </div>
+		        <div id="input-contend">
+			        <label>Describe cómo solucionaste el problema</label>
+			        <span>1000 caracteres restante</span>
+			        <input placeholder="(¿Cúal es tu solución?)"class="text" type="text">
+		        </div> 
+
 		        <label>Sube tu imagen o link de tu vídeo aquí</label>
-		        <input placeholder="(Arrastra imágenes aquí o cárgalas desde tu ordenador)"class="imagen" type="text">
-		        <label>Agregar vinculo de video</label>
-		        <input placeholder="http://"class="" type="text">
+			    <div class="imagen">imágenes aquí o <a href="#">cárgalas desde tu ordenador</a></div>
+
+		     	<div id="input-contend">
+		       		<span>youtube</span>
+			        <label>Agregar vinculo de video</label>
+			        <input placeholder="http://"class="" type="text">
+		        </div>
+
 		        <div class="show-captcha">
 		          
 		        </div>
