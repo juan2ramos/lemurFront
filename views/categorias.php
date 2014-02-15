a24d82a4b306328233b8d85a62f6e380131b5118
juan2ramos <juan2ramos@gmail.com>
2014-02-15 16:10:23 -0500

    categorias

--- views/categorias.php
+++ views/categorias.php
@@ -1,11 +1,13 @@
 				        <div class="categorias">
                     <h2>Categorías</h2>
                     <p>Estas son las categorías vigentes para que puedas votar por tus ideas favoritas.</p>
-                    <a href="vota-por-una-idea" class="link activa">
+                    <a href="vota-por-una-idea" class="link">
+                      <img src="images/activa.png" class="activa">
                       <div class="contend-img"><img src="images/general.png"></div>
                       <div class="contend-p"><p>Nombre categoría</p></div>
                     </a>
-                    <a href="vota-por-una-idea" class="link activa">
+                    <a href="vota-por-una-idea" class="link">
+                      <img src="images/activa.png" class="activa">
                       <div class="contend-img"><img src="images/electrodomesticos.png"></div>
                       <div class="contend-p"><p>Nombre categoría</p></div>
                     </a>
