0832e90f8ec8b5d269a9e193608e618ebdf1e94d
juan2ramos <juan2ramos@gmail.com>
2014-02-15 17:29:25 -0500

    cambios vota

--- views/vota-por-una-idea.php
+++ views/vota-por-una-idea.php
@@ -1,3 +1,4 @@
+		<div class="back"><a href="categorias">Volver</a></div>
 		<div class="vota-por-una-idea">
 	      <h2>Vota por una idea</h2>
 	      <p>Elige tu favorita y ayudala con tu voto, encontrarás información detallada de cada idea donde podrás dejar tus comentarios.</p>
