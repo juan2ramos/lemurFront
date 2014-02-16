5192442d635240a74f6d72e12efcea5be01ea340
Juan <juan2ramos@MacBook-Pro-de-Juan.local>
2014-02-15 20:09:20 -0500

    password

---
+++ views/password.php
@@ -0,0 +1,11 @@
+				<div class="password">
+                    <h2>¿Olvidaste tu contraseña?</h2>
+                    <p>	Ingresa tu dirección de correo electrónico y te enviaremos instrucciones 
+                    	para generar una nueva contraseña.
+                    </p>
+                    <form>
+                    	<label>Correo electrónicos<span>*</span></label>
+                    	<input type="text">		
+                    	<input type="submit">
+                    </form>
+                </div> 
