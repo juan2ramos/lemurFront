e802eb779ecdcf4e957aed75e82f34ea331134b0
Michael Martinez <info@mi-martinez.com>
2014-02-17 11:53:28 -0500

    PHP - Texto

--- views/vota-por-una-idea.php
+++ views/vota-por-una-idea.php
@@ -1,7 +1,7 @@
 		<div class="back"><a href="categorias">Volver</a></div>
 		<div class="vota-por-una-idea">
 	      <h2>Vota por una idea</h2>
-	      <p>Elige tu favorita y ayudala con tu voto, encontrarás información detallada de cada idea donde podrás dejar tus comentarios.</p>
+	      <p>Elige tu idea favorita y ayudala con tu voto, encontrarás información detallada de cada idea donde podrás dejar tus comentarios.</p>
 	      <ul>
 	        <li>
 	          <div class="izq">78</div>
