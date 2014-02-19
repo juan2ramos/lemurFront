b764e2732b20addd66c88e27a31db1c68b07cbb3
mi-martinez <soporte@mi-martinez.com>
2014-02-18 22:02:44 -0500

    PHP preguntas

--- views/preguntas.php
+++ views/preguntas.php
@@ -11,33 +11,40 @@
 					<dd>
 						Tienes una idea de producto, la dibujas y le tomas una foto o la explicas por medio de un video, te registras en nuestra plataforma, subes tu idea, consigues la mayor cantidad de votos, el equipo de Lemur Studio te elige como idea ganadora, diseña tu idea, la produce, la vende y tu recibes un porcentaje por cada producto vendido.
 					</dd>
+					<dt><a href="" class="expand">¿Qué clase de ideas puedo subir?</a></dt>
+					<dd>
+						La que quieras, siempre y cuando sea una idea de producto. 
+					</dd>
 					<dt><a href="" class="expand">¿Cómo puedo participar?</a></dt>
 					<dd>
-						Dolor sit amet, consectetur adipiscing elit. Aliquam vitae fringilla augue. 
-						Maecenas in lectus lorem. In et accumsan mi. Aenean vestibulum nisl eu arcu 
-						viverra iaculis. In hac habitasse platea dictumst. In vehicula diam et 
-						mauris imperdiet aliquet.
-					</dd>
-
-					<dt><a href="" class="expand">¿Porqué este negocio?</a></dt>
-					<dd>
-						Dolor sit amet, consectetur adipiscing elit. Aliquam vitae fringilla augue. 
-						Maecenas in lectus lorem. In et accumsan mi. Aenean vestibulum nisl eu arcu 
-						viverra iaculis. In hac habitasse platea dictumst. In vehicula diam et 
-						mauris imperdiet aliquet.
+						 Es muy fácil, regístrate, sube tu idea de producto de consumo, consigue con tus amigos la mayor cantidad de votos, para esto ellos también deben estar registrados, y espera que el grupo de Lemur Studio elija tu idea como ganadora, entonces, te lo haremos saber.
 					</dd>
 
 					<dt><a href="" class="expand">¿Cómo funciona el sistema de regalías?</a></dt>
 					<dd>
-						Dolor sit amet, consectetur adipiscing elit. Aliquam vitae fringilla augue. 
-						Maecenas in lectus lorem. In et accumsan mi. Aenean vestibulum nisl eu arcu 
-						viverra iaculis. In hac habitasse platea dictumst. In vehicula diam et 
-						mauris imperdiet aliquet.
+						En el momento que tu idea sea la ganadora, el equipo de Lemur Studio la diseñará, desarrollará y venderá. Por la venta de cada producto, tú recibirás un porcentaje durante el tiempo que éste sea vendido, y será consignado el valor cada mes en la cuenta bancaria que nos indiques.
 					</dd>
-
+					<dt><a href="" class="expand">¿Cómo se escoge la idea ganadora?</a></dt>
+					<dd>
+						Después de subir tu idea, debes conseguir la mayor cantidad de votos, al finalizar la votación, se escogerán las 3 ideas con mayor cantidad de puntos y de éstas 3, el equipo de Lemur Studio elegirá la idea que finalmente diseñara, producirá y comercializará
+					</dd>
+					<dt><a href="" class="expand">¿Y qué gano yo?</a></dt>
+					<dd>
+						Aparte de las regalías que recibirás durante el tiempo de venta del producto, en cada empaque vendrá un reconocimiento para que las personas que lo adquieran sepan que la idea vino de tí.  
+					</dd>
+					<dt><a href="" class="expand">¿Cómo se que he sido el/la ganador/a?</a></dt>
+					<dd>
+						Cuando el equipo de Lemur Studio te elija como idea ganadora, pasado el tiempo de las votaciones,  te enviaremos una notificación a tu correo electrónico y posteriormente nos comunicaremos contigo para empezar todo el proceso interno para la creación de tu idea de producto.  
+					</dd>
+					<dt><a href="" class="expand">¿Tienes más preguntas?</a></dt>
+					<dd>
+						Escríbenos y te responderemos cuanto antes.  
+					</dd>
+					<dt><a href="" class="expand">¿Cómo subo mi video?</a></dt>
+					<dd>
+						Sencillo, sólo colocas el link de tu video de youtube en el menú “sube tu idea” y ya está. 
+					</dd>
 					<dt><a href="terminos-y-condiciones" >Términos y Condiciones</a></dt>
-
-
 					<dt><a href="contacto" >Más preguntas</a></dt>
 
 				</dl>
