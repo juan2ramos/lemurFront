76a1fb4a18f238d1e5f201eac6362aa25ce0c009
mi-martinez <soporte@mi-martinez.com>
2014-02-18 21:52:44 -0500

    Textos

--- views/preguntas.php
+++ views/preguntas.php
@@ -4,12 +4,13 @@
 
 					<dt><a href="" class="expand">¿Qué es Lemur Studio?</a></dt>
 					<dd>
-						Dolor sit amet, consectetur adipiscing elit. Aliquam vitae fringilla augue. 
-						Maecenas in lectus lorem. In et accumsan mi. Aenean vestibulum nisl eu arcu 
-						viverra iaculis. In hac habitasse platea dictumst. In vehicula diam et 
-						mauris imperdiet aliquet.
+						Somos un estudio de diseño, con  3 propósitos claros:  El primero: Dar soluciones de diseño a nuestros clientes y comunidad sobrepasando sus expectativas. El segundo: Apoyar a los colombianos creativos y recursivos por medio del diseño a hacer realidad sus ideas de producto y retribuirles económicamente. 
+						El tercero: Aportar por medio del diseño, al desarrollo del país. 
 					</dd>
-
+					<dt><a href="" class="expand">¿De qué se trata esto?</a></dt>
+					<dd>
+						Tienes una idea de producto, la dibujas y le tomas una foto o la explicas por medio de un video, te registras en nuestra plataforma, subes tu idea, consigues la mayor cantidad de votos, el equipo de Lemur Studio te elige como idea ganadora, diseña tu idea, la produce, la vende y tu recibes un porcentaje por cada producto vendido.
+					</dd>
 					<dt><a href="" class="expand">¿Cómo puedo participar?</a></dt>
 					<dd>
 						Dolor sit amet, consectetur adipiscing elit. Aliquam vitae fringilla augue. 
